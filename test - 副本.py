00329f75a79af851a50478e7bb18d575aadf6bf6
obgnail <he1251698542@gmail.com>
2021-09-12 14:56:43 +0800

    Delete test - 副本.py

--- test - 副本.py
+++
@@ -1,7 +0,0 @@
-import time
-import hashlib
-def md5(data):
-    data += ('a3dcb4d229de6fde0db5686dee47145d' + str(time.time()))
-    return hashlib.md5(data.encode(encoding='UTF-8')).hexdigest()
-
-print(md5('何应良'))
